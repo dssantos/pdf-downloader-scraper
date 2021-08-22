ddebcf7d6fd2e75b01ab9d113da97963937cd322
dssantos <danilosantos1986@gmail.com>
2021-08-22 01:24:40 -0300

    Implements text extractor from PDF

--- pdf-downloader-scraper.py
+++ pdf-downloader-scraper.py
@@ -1,22 +1,25 @@
 from time import sleep
-from os.path import dirname, abspath
-import traceback
+from os import listdir
+from os.path import dirname, abspath, getctime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import SessionNotCreatedException, WebDriverException, NoSuchElementException
 from decouple import config, Csv
+import pdfplumber
 
-def last_file():
+def last_file(download_path):
     '''Must return the filename of the most recent file in the folder'''
-    return 'filename'
+    files = [download_path + x for x in listdir(download_path) if x.endswith(".pdf")]
+    newest = max(files , key = getctime)
+    return newest
 
 def pdf_downloader():
     '''Download file in especified folder after 
     scraping an element in headless mode and click on it'''
     try:
         # webdriver definitions
-        download_path = dirname(abspath(__file__)) + '/downloads'
+        download_path = dirname(abspath(__file__)) + '/downloads/'
         prefs = {"download.default_directory":download_path}
         options = webdriver.ChromeOptions()
         options.binary_location = '/usr/bin/google-chrome'
@@ -48,30 +51,40 @@ def pdf_downloader():
 
         # wait download finish and close browser
         filename = f'diario_{number}_{date}.pdf'
-        for atempt in range(1,6):
-            if filename is not last_file():
-                sleep(2)
+        waiting_download = True
+        atempt = 0
+        while waiting_download:
+            if atempt < 30:
+                waiting_download = filename not in listdir(download_path)
+                sleep(1)
+                atempt += 1
             else:
                 return 'Error', '', f'Time out. Download error after {atempt} atempts'
         print(f'File {filename} downloaded')
         driver.close()
 
     except (
+        AttributeError,
         SessionNotCreatedException, 
         WebDriverException, 
         NoSuchElementException) as e:
         return 'Error', '', e
 
     return 'Success', filename, ''
-    
 
-def read_pdf(filename):
-    '''Must return a file object from a filename'''
-    return 'file object'
-
-def pdf_scraper(pdf_file):
+def pdf_scraper(file_path):
     '''Must return a raw text of the PDF file'''
-    return 'raw text'
+    file_path = dirname(abspath(__file__)) + '/downloads/diario_000548_20210820.pdf'
+    pdf = pdfplumber.open(file_path)
+    texts = []
+    for num in range(len(pdf.pages)):
+        page = pdf.pages[num]
+        l_page = page.crop((0, 0.1 * float(page.height), 0.5 * float(page.width), 0.93 * float(page.height)))
+        texts.append(l_page.extract_text())
+        r_page = page.crop((0.5 * float(page.width), 0.1 * float(page.height), page.width, 0.93 * float(page.height)))
+        texts.append(r_page.extract_text())
+    text = '\n'.join(texts)
+    return text
 
 def send_mail(receiver_email, subject, msg):
     '''Sends a personalized message to specified email list'''
@@ -81,15 +94,16 @@ def send_mail(receiver_email, subject, msg):
 email_list = config('EMAIL_LIST', cast=Csv())
 status, filename, message_error = pdf_downloader()
 if status == 'Success':
-    pdf_file = read_pdf(filename)
-    pdf_content = pdf_scraper(pdf_file)
+    file_path = dirname(abspath(__file__)) + '/downloads/' + filename
+    pdf_content = pdf_scraper(file_path)
     text_list = config('TEXT_LIST', cast=Csv())
-    if any(text_to_find.lower() in pdf_content.lower() for text_to_find in text_list):
+    raw_text = pdf_content.lower().replace('  ', ' ').replace('\n', ' ')
+    if any(text_to_find.lower() in raw_text for text_to_find in text_list):
         for text_to_find in text_list:
-            if text_to_find.lower() in pdf_content.lower():
+            if text_to_find.lower() in raw_text:
                 msg = f'{text_to_find} found in {filename}'
                 send_mail(email_list, 'New updates', msg)
-                print(f'Sending {msg} to {", ".join(email_list)}')
+                print(f'Sending mail: {msg} to {", ".join(email_list)}')
     else:
         print('Texts not found:\n'+"\n".join(text_list))
 else:
